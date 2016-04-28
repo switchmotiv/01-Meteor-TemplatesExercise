08909447cdcf8b3e328fde1e3f7549cd5547bb99
switchmotiv <j.alfonso84@gmail.com>
2016-04-28 10:39:22 -0500

    Submissions
    Started setting up submissions

---
+++ client/templates/posts/post_submit.js
@@ -0,0 +1,14 @@
+Template.postSubmit.events ({
+  // submit is better than click because it covers all ways of submitting such as hitting 'enter'
+  'submit form': function(e) {
+    e.preventDefault();
+
+    var post = {
+      url: $(e.target).find('[name=url]').val(),
+      title: $(e.target).find('[name=title]').val()
+    };
+
+    post._id = Posts.insert(post);
+    Router.go('postPage', post);
+  }
+});
