803c58a1e0c1144af5e43c7e56739d8001216539
switchmotiv <j.alfonso84@gmail.com>
2016-05-12 22:09:54 -0500

    Setup post validation
    post validation

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -8,7 +8,28 @@ Template.postSubmit.events ({
       title: $(e.target).find('[name=title]').val()
     };
 
-    post._id = Posts.insert(post);
-    Router.go('postPage', post);
+    // The Meteor.call function calls a Method named by its first argument.
+    // You can provide arguments to the call (in this case, the post object we constructed from the form),
+    // and finally attach a callback, which will execute when the server-side Method is done.
+
+    // Meteor method callbacks always have two arguments, error and result.
+    // If for whatever reason the error argument exists, we'll alert the user (using return to abort the callback).
+    // If everything's working as it should, we'll redirect the user to the freshly created post's discussion page.
+    // postInsert is defined in lib/collections/posts.js
+    Meteor.call('postInsert', post, function(error, result) {
+      // display the error to the user an abort
+      if (error)
+        return alert(error.reason);
+
+      if (result.postExists)
+        alert('This link has already been posted');
+
+      Router.go('postPage', {_id: result._id});
+    });
+    // The insert() function on a collection returns the generated _id for the object that has been
+    // inserted into the database, which the Router's go() function will use to construct a URL for us to browse to.
+
   }
+  // The net result is the user hits submit, a post is created, and the user is instantly taken to the
+  // discussion page for that new post.
 });
