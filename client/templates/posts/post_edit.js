70b70bdeb51c735bc254c04b82838d813016b63a
switchmotiv <j.alfonso84@gmail.com>
2016-05-13 15:14:29 -0500

    Permissions
    Added basic permission to check the post's owner.

---
+++ client/templates/posts/post_edit.js
@@ -0,0 +1,34 @@
+Template.postEdit.events({
+  'submit form': function(e) {
+    // prevent the default event
+    e.preventDefault();
+
+    // get the current post
+    var currentPostId = this._id;
+
+    // get the new form field values from the page and store them in a postProperties object.
+    var postProperties = {
+      url: $(e.target).find('[name=url]').val(),
+      title: $(e.target).find('[name=title]').val()
+    }
+
+    Posts.update(currentPostId, {$set: postProperties}, function(error) {
+      if (error) {
+        // display error to the user
+        alert(error.reason);
+      } else {
+        Router.go('postPage', {_id: currentPostId});
+      }
+    });
+  },
+
+  'click .delete': function(e) {
+    e.preventDefault();
+
+    if (confirm("Deletes this post?")) {
+      var currentPostId = this._id;
+      Posts.remove(currentPostId);
+      Router.go('postsList');
+    }
+  }
+});
