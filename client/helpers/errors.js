92db6d4de01a0c0e96c9ae02a33426a2277aa477
switchmotiv <j.alfonso84@gmail.com>
2016-05-19 13:18:43 -0500

    Errors
    Ironically this throws an error.

--- client/helpers/errors.js
+++
@@ -1,8 +0,0 @@
-// Local (client-only) Collection
-// The advantage of using a local collection to store the errors is that, like all collections,
-// it's reactive -- meaning we can reactively display errors in the same way we display any other collection data.
-Errors = new Mongo.Collection(null);
-
-throwError = function(message) {
-  Errors.insert({message: message});
-};
